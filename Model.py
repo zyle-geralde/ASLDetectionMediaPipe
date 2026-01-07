0ce593ee86f1320152e2bfd527033b9c8e089780
zyle-geralde <zylgerdlp19@gmail.com>
2026-01-08 00:04:51 +0800

    the thoughts are coming back agian

--- Model.py
+++ Model.py
@@ -1,31 +1,35 @@
 from pathlib import Path
-
+import numpy as np
 import cv2
 import mediapipe as mp
+
 
 #Initializing Hands
 mp_hands = mp.solutions.hands#responsoble for detecting hands and 21 landmarks
 hands = mp_hands.Hands(
     static_image_mode=True, #tells mediapipe that the input is a single image, not a video stream
     max_num_hands=2,# maximum number of hands detected
-    min_detection_confidence=0.5, #Detection must be 50% confident to be accepted. If the detected object has below 50% confidence score, it will ignore it
+    min_detection_confidence=0.2, #Detection must be 50% confident to be accepted. If the detected object has below 50% confidence score, it will ignore it
 )
 
 mp_drawing = mp.solutions.drawing_utils # utility function for drawing landmarks etc.
 
 valid_file = {".jpg", ".jpeg", ".png"}
 
+x = [] # features (the x,y,z landmarks)
+y = [] # labels (a,b,c,d ...)
+
 #Pre processing - adding keypoints to ASL images
 path = Path("C://Users//zylge//Downloads//asl_dataset") # get the path of the dataset.
 
 
-count_for_Items = 0
 #loops through all files and subfolders insided tha path and returns the full path of each items
 for items in path.iterdir():
     image_folder_path = items
 
     #check if items is a directory
     if not items.is_dir():
+        print("Is not a directory")
         continue
 
     #loops through every image or subfolder of the current subfolder and returns the full path of each items
@@ -33,6 +37,7 @@ for items in path.iterdir():
 
         #skip non-image file
         if image_items.suffix.lower() not in valid_file:
+            print("non-image file")
             continue
 
         image_paths = str(image_items)
@@ -41,6 +46,7 @@ for items in path.iterdir():
 
         #skip unreadable images
         if read_image is None:
+            print("unreadable image")
             continue
 
         image_rgb = cv2.cvtColor(read_image, cv2.COLOR_BGR2RGB)# Convert BGR to RGB format. Open cv reads images as BGR format. Mediapipe expects RGB format
@@ -54,25 +60,36 @@ for items in path.iterdir():
         #Check if at least one hand exists
         if results.multi_hand_landmarks: # returns one or two lists containing 21 landmarks depending on how many hands detected
             for hands_idx in results.multi_hand_landmarks:
+                #list of compiled landmarks of the hand
+                compiled_features = []
                 # loops the landmarks
                 for index,hand_landmark in enumerate(hands_idx.landmark):#hand_landmark is a tuple or list of x,y,z
                     h,w,_ = read_image.shape
                     #since x,y,z are normalized (between 0 -1) multiply it with width and height respectively
-                    print(hand_landmark.x * w, hand_landmark.y * h, hand_landmark.z)
+                    compiled_features.extend([hand_landmark.x,hand_landmark.y,hand_landmark.z])
+                    #print(hand_landmark.x * w, hand_landmark.y * h, hand_landmark.z)
 
-                # draw landmarks on images.
-                mp_drawing.draw_landmarks(read_image,hands_idx,mp_hands.HAND_CONNECTIONS)
+                #Check the length of compiled_feature list(should be 63 -> 21*3)
+                if len(compiled_features) == 63:
+                    x.append(compiled_features)
+                    y.append(image_folder_path.name)
+                if len(compiled_features) != 63:
+                    print("compiled feature != 63")
+
+                #Optional for diagram: draw landmarks on images.
+                #mp_drawing.draw_landmarks(read_image,hands_idx,mp_hands.HAND_CONNECTIONS)
+
+        else:
+            print("Did not detect hand "+str(image_items))
 
 
-        # Optional for checking: Show result
+        # Optional for diagram: Show result
         # cv2.imshow("Hand Landmarks", read_image)
         # cv2.waitKey(0)
         # cv2.destroyAllWindows()
 
-        count_for_Items+=1
-
-print(count_for_Items)
-
+print(len(x))
+print(len(y))
 
 
 
