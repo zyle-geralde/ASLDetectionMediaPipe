32f6209287e688e087152e3089a104f2c6912b02
zyle-geralde <zylgerdlp19@gmail.com>
2026-01-07 21:46:31 +0800

    fixed errors on landmark extraction

--- Model.py
+++ Model.py
@@ -13,19 +13,36 @@ hands = mp_hands.Hands(
 
 mp_drawing = mp.solutions.drawing_utils # utility function for drawing landmarks etc.
 
+valid_file = {".jpg", ".jpeg", ".png"}
+
 #Pre processing - adding keypoints to ASL images
 path = Path("C://Users//zylge//Downloads//asl_dataset") # get the path of the dataset.
 
 
+count_for_Items = 0
 #loops through all files and subfolders insided tha path and returns the full path of each items
 for items in path.iterdir():
     image_folder_path = items
 
+    #check if items is a directory
+    if not items.is_dir():
+        continue
+
     #loops through every image or subfolder of the current subfolder and returns the full path of each items
     for image_items in image_folder_path.iterdir():
+
+        #skip non-image file
+        if image_items.suffix.lower() not in valid_file:
+            continue
+
         image_paths = str(image_items)
 
         read_image = cv2.imread(image_paths)#load image
+
+        #skip unreadable images
+        if read_image is None:
+            continue
+
         image_rgb = cv2.cvtColor(read_image, cv2.COLOR_BGR2RGB)# Convert BGR to RGB format. Open cv reads images as BGR format. Mediapipe expects RGB format
 
         #process image
@@ -36,23 +53,26 @@ for items in path.iterdir():
 
         #Check if at least one hand exists
         if results.multi_hand_landmarks: # returns one or two lists containing 21 landmarks depending on how many hands detected
-            for hands in results.multi_hand_landmarks:
+            for hands_idx in results.multi_hand_landmarks:
                 # loops the landmarks
-                for index,hand_landmark in enumerate(hands.landmark):#hand_landmark is a tuple or list of x,y,z
+                for index,hand_landmark in enumerate(hands_idx.landmark):#hand_landmark is a tuple or list of x,y,z
                     h,w,_ = read_image.shape
                     #since x,y,z are normalized (between 0 -1) multiply it with width and height respectively
                     print(hand_landmark.x * w, hand_landmark.y * h, hand_landmark.z)
 
                 # draw landmarks on images.
-                mp_drawing.draw_landmarks(read_image,hands,mp_hands.HAND_CONNECTIONS)
+                mp_drawing.draw_landmarks(read_image,hands_idx,mp_hands.HAND_CONNECTIONS)
 
-        # Show result
-        cv2.imshow("Hand Landmarks", read_image)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
-        break
 
-    break
+        # Optional for checking: Show result
+        # cv2.imshow("Hand Landmarks", read_image)
+        # cv2.waitKey(0)
+        # cv2.destroyAllWindows()
+
+        count_for_Items+=1
+
+print(count_for_Items)
+
 
 
 
