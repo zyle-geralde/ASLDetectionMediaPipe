16928046afce58ce994adb346a5a5a268bb15507
zyle-geralde <zylgerdlp19@gmail.com>
2026-01-09 12:55:41 +0800

    added detection mediapipe detection

--- VideoImplementation.py
+++ VideoImplementation.py
@@ -1,4 +1,16 @@
 import cv2
+import mediapipe as mp
+import numpy as np
+
+#Hand landmark detection
+mp_hands = mp.solutions.hands
+hands = mp_hands.Hands(
+    static_image_mode=False,
+    max_num_hands=2,
+    min_detection_confidence=0.5,
+)
+
+mp_drawing = mp.solutions.drawing_utils
 
 cap = cv2.VideoCapture(0) # Open default camera
 
@@ -14,6 +26,14 @@ while True:
     if not ret:
         print("Frame not captured")
         break
+
+    frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+    result_frame = hands.process(frame_rgb)
+
+    if result_frame.multi_hand_landmarks:
+        for hands_idx in result_frame.multi_hand_landmarks:
+            mp_drawing.draw_landmarks(frame, hands_idx, mp_hands.HAND_CONNECTIONS)
 
     #Show frame
     cv2.imshow("Webcam",frame)
